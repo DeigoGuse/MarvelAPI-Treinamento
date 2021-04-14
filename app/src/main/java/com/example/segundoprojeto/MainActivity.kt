2fe8cdf5cbc696e5946fbd6cb839942f8af5030a
DiegoGuse <diego.guse@lg.com.br>
2021-04-14 14:29:48 -0300

    Inicio dos trabalhos, botoes de entrar e sair , background de entrada .

--- app/src/main/java/com/example/segundoprojeto/MainActivity.kt
+++ app/src/main/java/com/example/segundoprojeto/MainActivity.kt
@@ -1,11 +1,12 @@
 package com.example.segundoprojeto
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        supportActionBar?.hide()
         setContentView(R.layout.activity_main)
     }
 }
